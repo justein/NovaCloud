0b28092b66d51baf100f228dbc90feffe433b35e
Lyn <james_gosling@163.com>
2019-02-26 10:45:36 +0800

    添加注释

--- netty-springboot-server/src/main/java/com/nova/sbnetty/monitor/EndpointConfig.java
+++ netty-springboot-server/src/main/java/com/nova/sbnetty/monitor/EndpointConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 /***
  * @ClassName: EndpointConfig
- * @Description: TODO
+ * @Description: 构造客户端通道缓存
  * @Author: Lyn
  * @Date: 2019/2/24 下午9:17
  * @version : V1.0
